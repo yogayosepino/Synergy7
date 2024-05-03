8f25a5141e9b7afb75929861a69f259423ecbfc1
yogayosepino <85721439+yogayosepino@users.noreply.github.com>
2024-05-03 20:29:29 +0700

    Ch4

---
+++ F-BJV24001115-synergy7-yoga-binfood-ch4/src/main/java/com/example/Binarfood/BinarfoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.Binarfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinarfoodApplication.class, args);
+	}
+
+}
---
+++ F-BJV24001115-synergy7-yoga-binfood-ch4/src/test/java/com/example/Binarfood/BinarfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Binarfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinarfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
